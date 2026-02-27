32e295d48e7284b09a2bbf3ac2c183d2b66d4e6f
sofia <sofiagracia000@gmail.com>
2026-02-27 17:42:20 +0100

    vue.config.ts modified

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+module.exports = {
+  devServer: {
+    client:{
+      WebSocket: {
+        protocol: 'wss',
+        hostname: '0.0.0.0',
+        port: 443,
+      }
+    }
+  },
+  defineConfig: {
+    transpileDependencies: true
+  }
+}
